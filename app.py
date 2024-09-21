185618998bb03938a99cd8327866da2bacc18bbd
hamzaaleem2003 <125291397+hamzaaleem2003@users.noreply.github.com>
2024-09-21 16:14:24 +0500

    last commit

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chai
 store = {}
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
-    store[session_id] = ChatMessageHistory()
+      store[session_id] = ChatMessageHistory()
     return store[session_id]
 
 conversational_rag_chain = RunnableWithMessageHistory(
