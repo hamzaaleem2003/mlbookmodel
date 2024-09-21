3d667f54191209e4bd4092530161429a1c58675b
hamzaaleem2003 <125291397+hamzaaleem2003@users.noreply.github.com>
2024-09-21 16:13:24 +0500

    last commit

--- app.py
+++ app.py
@@ -20,90 +20,89 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
+
+# Initialize the API key for Google Generative AI
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-class ChatBot():
-    def __init__(self):
-        # Initialize the API key for Google Generative AI
-        GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-        # Initialize the Google Generative AI model
-        self.llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=GOOGLE_API_KEY)
-        # Load the embeddings model
-        embeddings = HuggingFaceEmbeddings()
+# Initialize the Google Generative AI model
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=GOOGLE_API_KEY)
+# Load the embeddings model
+embeddings = HuggingFaceEmbeddings()
+
+# Define the collection name and persistent directory for Chroma
+collection_name = "MLbookcollection"
+persist_directory = "MLbookcollection"  # Specify the persistent directory
+
+# Initialize Chroma by loading the existing collection
+knowledge = Chroma(
+    collection_name=collection_name,
+    persist_directory=persist_directory,
+    embedding_function=embeddings
+)
+retriever = knowledge.as_retriever()
+
+### Contextualize question ###
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
         
-        # Define the collection name and persistent directory for Chroma
-        collection_name = "MLbookcollection"
-        persist_directory = "MLbookcollection"  # Specify the persistent directory
-
-        # Initialize Chroma by loading the existing collection
-        self.knowledge = Chroma(
-            collection_name=collection_name,
-            persist_directory=persist_directory,
-            embedding_function=embeddings
-        )
-        retriever = self.knowledge.as_retriever()
-
-        ### Contextualize question ###
-        self.contextualize_q_system_prompt = (
-          "Given a chat history and the latest user question "
-          "which might reference context in the chat history, "
-          "formulate a standalone question which can be understood "
-          "without the chat history. Do NOT answer the question, "
-          "just reformulate it if needed and otherwise return it as is."
-        )
-        self.contextualize_q_prompt = ChatPromptTemplate.from_messages(
-           [
-               
-             ("system", self.contextualize_q_system_prompt),
-             MessagesPlaceholder("chat_history"),
-             ("human", "{input}"),
-           ]
-        )
-        history_aware_retriever = create_history_aware_retriever(
-        self.llm, retriever, self.contextualize_q_prompt
-        )
-
-
-
-
-        # 2. Incorporate the retriever into a question-answering chain.
-        self.system_prompt = (
-                        '''
-                        this is the data from the book and name of the book is "HandsOn Machine Learning with ScikitLearn Keras and TensorFlow 3rd Edition", I give u access to all the data in this book , whatever question is asked you have to answer that properly and comprehensively and in detail, whenever a question is asked from this book you always have to answer the question in English language no matter if in prompt it mentions to answer in English or not, but if it specifies to answer in some other language, only then you have to change the language in giving a response.
-                        '''
-                        "{context}"
-                        )
-        self.qa_prompt = ChatPromptTemplate.from_messages(
-           [
-        ("system", self.system_prompt),
+        ("system", contextualize_q_system_prompt),
         MessagesPlaceholder("chat_history"),
         ("human", "{input}"),
-           ]
-        )
-        self.question_answer_chain = create_stuff_documents_chain(self.llm, self.qa_prompt)
-        self.rag_chain = create_retrieval_chain(history_aware_retriever, self.question_answer_chain)
-        ### Statefully manage chat history ###
-        self.store = {}
-        def get_session_history(session_id: str) -> BaseChatMessageHistory:
-          if session_id not in self.store:
-            self.store[session_id] = ChatMessageHistory()
-          return self.store[session_id]
-        
-        self.conversational_rag_chain = RunnableWithMessageHistory(
-            self.rag_chain,
-            get_session_history,
-            input_messages_key="input",
-            history_messages_key="chat_history",
-            output_messages_key="answer",
-        )
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+llm, retriever, contextualize_q_prompt
+)
+
+
+
+
+# 2. Incorporate the retriever into a question-answering chain.
+system_prompt = (
+                '''
+                this is the data from the book and name of the book is "HandsOn Machine Learning with ScikitLearn Keras and TensorFlow 3rd Edition", I give u access to all the data in this book , whatever question is asked you have to answer that properly and comprehensively and in detail, whenever a question is asked from this book you always have to answer the question in English language no matter if in prompt it mentions to answer in English or not, but if it specifies to answer in some other language, only then you have to change the language in giving a response.
+                '''
+                "\n\n"
+                "{context}"
+                )
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+("system", system_prompt),
+MessagesPlaceholder("chat_history"),
+("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+### Statefully manage chat history ###
+store = {}
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+    store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
 # Create an instance of the ChatBot class
-bot = ChatBot()
 st.set_page_config(page_title="ML Book Bot")
 with st.sidebar:
     st.title(' ML Book Bot')
 
 # Function for generating LLM response incrementally
 def generate_response_stream(input):
-    response = bot.conversational_rag_chain.invoke(
+    response = conversational_rag_chain.invoke(
         {"input": input},
     config={
         "configurable": {"session_id": "abc123"}
