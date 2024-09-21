64b9be059a6603fdf3a2367740793e9db206531a
hamzaaleem2003 <125291397+hamzaaleem2003@users.noreply.github.com>
2024-09-21 17:35:47 +0500

    last commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
@@ -19,6 +19,8 @@ import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+
 
 # Initialize the API key for Google Generative AI
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -49,27 +51,31 @@ contextualize_q_system_prompt = (
 )
 contextualize_q_prompt = ChatPromptTemplate.from_messages(
     [
+        
         ("system", contextualize_q_system_prompt),
         MessagesPlaceholder("chat_history"),
         ("human", "{input}"),
     ]
 )
 history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, contextualize_q_prompt
+llm, retriever, contextualize_q_prompt
 )
+
+
+
 
 # 2. Incorporate the retriever into a question-answering chain.
 system_prompt = (
-    '''
-    this is the data from the book and name of the book is "HandsOn Machine Learning with ScikitLearn Keras and TensorFlow 3rd Edition", I give u access to all the data in this book , whatever question is asked you have to answer that properly and comprehensively and in detail, whenever a question is asked from this book you always have to answer the question in English language no matter if in prompt it mentions to answer in English or not, but if it specifies to answer in some other language, only then you have to change the language in giving a response.
-    '''
-    "{context}"
-)
+                '''
+                this is the data from the book and name of the book is "HandsOn Machine Learning with ScikitLearn Keras and TensorFlow 3rd Edition", I give u access to all the data in this book , whatever question is asked you have to answer that properly and comprehensively and in detail, whenever a question is asked from this book you always have to answer the question in English language no matter if in prompt it mentions to answer in English or not, but if it specifies to answer in some other language, only then you have to change the language in giving a response.
+                '''
+                "{context}"
+                )
 qa_prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
+("system", system_prompt),
+MessagesPlaceholder("chat_history"),
+("human", "{input}"),
     ]
 )
 question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
@@ -78,7 +84,7 @@ rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chai
 store = {}
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
-        store[session_id] = ChatMessageHistory()
+      store[session_id] = ChatMessageHistory()
     return store[session_id]
 
 conversational_rag_chain = RunnableWithMessageHistory(
@@ -97,9 +103,9 @@ with st.sidebar:
 def generate_response_stream(input):
     response = conversational_rag_chain.invoke(
         {"input": input},
-        config={
-            "configurable": {"session_id": "abc123"}
-        },  # constructs a key "abc123" in `store`.
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in store.
     )["answer"]
     # Simulate streaming by yielding one character at a time
     for char in response:
