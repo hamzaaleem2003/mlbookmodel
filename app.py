29c5da5fcc93681f07e3e39edfbadc324491425b
hamzaaleem2003 <125291397+hamzaaleem2003@users.noreply.github.com>
2024-09-22 16:13:03 +0500

    last commit

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def inject_custom_css():
     <style>
     /* Main chat area background color */
     [data-testid="stAppViewContainer"] > .main {
-        background-color: #FFE0E0 ; /* Sky Blue */
+        background-color: #7F5959 ; /* Sky Blue */
 
     }
 
