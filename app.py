d79a13927e37a2ef95aa7e7c67b645202b1e5b0b
hamzaaleem2003 <125291397+hamzaaleem2003@users.noreply.github.com>
2024-09-22 15:27:47 +0500

    last commit

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ history_aware_retriever = create_history_aware_retriever(
 system_prompt = (
     '''
     This is the data from the book named "Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow 3rd Edition". I give you access to all the data in this book. Whatever question is asked, you have to answer it properly, comprehensively, and in detail. Whenever a question is asked from this book, you always have to answer the question in English language no matter if in prompt it mentions to answer in English or not, but if it specifies to answer in some other language, only then you have to change the language in giving a response.
-    One thing also you have to remember that your owner and the one who created you is "Mr Hamza Aleem" who is a student of electrical engineering at "NUST" in Pakistan, and he is an expert in microwave engineering and antenna design.
+    One thing also you have to remember that your owner and the one who created you is "Mr Hamza Aleem" who is a student of electrical engineering at "NUST" in Pakistan, and he is an expert in microwave engineering and antenna design and he is inspired by the person whose profile link is "https://ceme.nust.edu.pk/faculty/muhammad-anis-chaudhary/".
     '''
     "{context}"
 )
@@ -104,7 +104,7 @@ def inject_custom_css():
     <style>
     /* Main chat area background color */
     [data-testid="stAppViewContainer"] > .main {
-        background-color: #87CEEB; /* Sky Blue */
+        background-color: #ffe8c7; /* Sky Blue */
     }
 
     /* Sidebar background color */
@@ -153,7 +153,7 @@ def generate_response_stream(user_input):
 
 # 11. Store LLM generated responses
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "Welcome, ask me anything from your book"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome, ask me anything from this book"}]
 
 # 12. Display chat messages
 for message in st.session_state.messages:
